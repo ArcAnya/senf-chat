c803677bbd7c1b9bb0600b28f13676647cbe7cd5
Arc_Anya <arcanyatwitch@gmail.com>
2021-11-22 22:37:22 +0100

    messages logic added, but messages don't show

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { db } from '../firebase'
 import SignOut from './SignOut'
+import { query, orderBy, limit, collection, onSnapshot } from "firebase/firestore"; 
 
 function Chat() {
+    const [messages, setMessages] = useState([])
+    useEffect(() => {
+        const q = query( collection(db, 'messages'), orderBy('createdAt'), limit(50)) 
+        onSnapshot(q, (snapshot) => {
+            setMessages(snapshot.docs.map(doc => doc.data))
+        })
+    }, [])
     return (
         <div>
             <SignOut />
-            Chat
+            {messages.map(({id, text, photoURL}) => (
+                <div key={id}>
+                    <img src={photoURL} alt=''/>
+                    <p>{text}</p>
+                </div>
+            ))}
         </div>
     )
 }
